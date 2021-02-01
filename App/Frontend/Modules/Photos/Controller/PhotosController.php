4d98c9f22b667cbef24af8a421227d1f3c0832fe
Romain CHARLES <romain.charles@rocketmail.com>
2021-02-01 17:50:39 +0100

    Création du controller des photos

---
+++ App/Frontend/Modules/Photos/Controller/PhotosController.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Frontend\Modules\Photos\Controller;
+
+
+use Model\PhotosManager;
+use RCFramework\HTTPRequest;
+
+class PhotosController extends \RCFramework\BackController
+{
+    public function executeShowallphotos(HTTPRequest $request)
+    {
+        $photosManager = new PhotosManager();
+
+        $allPhotosData = $photosManager->getAllPhotos();
+
+        $this->page->addVar('photos', $allPhotosData);
+    }
+
+
+    public function executeShowOnePhoto(HTTPRequest $request)
+    {
+        
+    }
+}
