a3939236b1c579b6fa7e92ce240f3accef5297e5
Romain CHARLES <romain.charles@rocketmail.com>
2021-02-09 18:00:52 +0100

    Config messages d'erreur

--- Web/main.php
+++ Web/main.php
@@ -1,25 +1,39 @@
 <?php
+
+const DEFAULT_APP = 'Frontend';
+
 try
 {
     require_once __DIR__ . "/../lib/vendor/autoload.php";
 
+// Si l'application n'est pas valide, on va charger l'application par défaut qui se chargera de générer une erreur 404
+    if (!isset($_GET['app']) || !file_exists(__DIR__.'/../App/'.$_GET['app']))
+    {
+        $_GET['app'] = DEFAULT_APP;
+    }
 
+
+    // Il ne nous suffit plus qu'à déduire le nom de la classe et à l'instancier
+    $appClass = 'App\\'.$_GET['app'].'\\'.$_GET['app'].'Application';
+
+    $app = new $appClass;
+
+    $app->run();
 
 //\App\Test::affiche();
-
+/*
     $loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/../App');
     $twig = new \Twig\Environment($loader, [
         'cache' => false
     ]);
 
     echo $twig->render('Templates/general_layout.html.twig');
-    echo 'tototototo';
+    echo 'tototototo';*/
 }
 catch (Throwable $exception)
 {
     echo 'ERREUR : Merci de contacter le responsable du site';
-    ob_start();
-    var_dump($exception);
-    $vardumpcontent = ob_get_clean();
-    file_put_contents(__DIR__.'/../cache/erreur_'.date('Y-m-d').'.log', '['.date('H:i:s').'] '.$vardumpcontent, FILE_APPEND);
+    $message = 'EXCEPTION ['.get_class($exception).'] : '.$exception->getMessage().' ('.$exception->getFile().':'.$exception->getLine().')';
+    file_put_contents(__DIR__.'/../cache/erreur_'.date('Y-m-d').'.log', '['.date('H:i:s').'] '.$message.PHP_EOL, FILE_APPEND);
 }
+
