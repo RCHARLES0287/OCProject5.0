0bd1f01ca504236fd92fdd2e74d2dfe951de2040
Romain CHARLES <romain.charles@rocketmail.com>
2021-02-16 17:55:45 +0100

    Alimentation du menu général à partir de la BDD

--- App/Frontend/Modules/Accueil/Controller/AccueilController.php
+++ App/Frontend/Modules/Accueil/Controller/AccueilController.php
@@ -4,12 +4,16 @@
 namespace App\Frontend\Modules\Accueil\Controller;
 
 
+use Model\GaleriesManager;
 use RCFramework\HTTPRequest;
 
 class AccueilController extends \RCFramework\BackController
 {
     public function executeAccueil(HTTPRequest $request)
     {
+        $galeriesManager = new GaleriesManager();
+        $allGaleries = $galeriesManager->getAllGaleries();
 
+        $this->page->addVar('allGaleries', $allGaleries);
     }
 }
