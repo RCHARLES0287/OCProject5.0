e1605b0b9a78bf9a285078c0b36ad87eed38fa70
Romain CHARLES <romain.charles@rocketmail.com>
2021-04-13 18:10:02 +0200

    Validation d'un article avec requête Ajax (en cours)

--- Web/js/showOneArticle.js
+++ Web/js/showOneArticle.js
@@ -1,70 +1,21 @@
-/*
-
-$('#dimensions_deroulant').change(function (event){
-    // Ciblage de l'attribut "value" que l'on passe en paramètre à la nouvelle instance de MenuDeroulantSelection
-    new PlacingAssociatedValue($('option[value="'+$(this).val()+'"]', $(this)).data('prix'), 'prix_article');
-    console.log();
-    return true;
-});
-
-
-// $(this).data('prix')
-
-$('#toto').change(function (event){
-    // Ciblage de l'attribut "value" que l'on passe en paramètre à la nouvelle instance de MenuDeroulantSelection
-    new PlacingAssociatedValue($('option[value="'+$(this).val()+'"]', $(this)).data('tata'), 'tutu');
-    console.log();
-    return true;
-})
-
-
-// Ne peut travailler qu'avec un select '
-new Nouvelleclasse ('#toto', 'tata', '#tutu');
-
-
-new NouvelleClasse ('#dimensions_deroulant', 'prix', '#prix_article');
-*/
-/*
-
-$('#dimensions_deroulant').change(function (event){
-   new PlacingAssociatedValue('#dimensions_deroulant', 'prix', 'prix_article');
-});
-*/
 
 new PlacingAssociatedValue('#dimensions_deroulant', 'prix', '#prix_article');
 
-/*
 
-class NouvelleClasse {
-    constructor(objetSelect, associatedValue, newLocation) {
 
-        let joinedValue = $('option[value="'+$(objetSelect).val()+'"]', $(objetSelect)).data(associatedValue);
-        let newPlace = newLocation;
+$("#validation_article").click(function(e){
+    e.preventDefault();
+    console.log('On est entré dans la requête ajax pour la validation du formulaire')
 
-        this.placeAssociatedValue();
-    }
+    $.ajax({
+        url : '/validateonearticle', // La ressource ciblée
+        type : 'POST', // Le type de la requête HTTP
 
-    placeAssociatedValue () {
-        console.log('La fonction placeAssociatedValue se lance correctemennt');
-        console.log(this.place);
-        $('.place').text($(this).joinedValue);
-    }
-}
+        data : 'idPhoto=' + id_photo + 'idDimensions' + id_dimensions + 'nombreArticles' + nombre_articles,
+        // dataType : 'json' // Le type de données à recevoir, ici, du json.
+    });
+    console.log('La requête ajax pour la validation du formulaire fonctionne')
 
-class PlacingAssociatedValue {
-    constructor(value, associatedValue, newLocation) {
-        // const menuValue = value;
-        let initialValue = value;
-        let joinedValue = associatedValue;
-        let place = newLocation;
-        console.log(initialValue, joinedValue, place);
-        this.placeAssociatedValue();
-    }
+});
 
-    placeAssociatedValue () {
-        console.log('La fonction placeAssociatedValue se lance correctemennt');
-        console.log(this.place);
-        $('.place').text($(this).joinedValue);
-    }
-}
-*/
+
