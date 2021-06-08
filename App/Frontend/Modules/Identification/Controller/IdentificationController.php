3a3573a1156eb1d2667dddcc77dfb0287478bec4
Romain CHARLES <romain.charles@rocketmail.com>
2021-06-08 18:12:43 +0200

    Identification du visiteur

--- App/Frontend/Modules/Identification/Controller/IdentificationController.php
+++ App/Frontend/Modules/Identification/Controller/IdentificationController.php
@@ -3,6 +3,7 @@
 
 namespace App\Frontend\Modules\Identification\Controller;
 
+use Entity\UtilisateurEntity;
 use Model\UtilisateursManager;
 use RCFramework\HTTPRequest;
 
@@ -23,6 +24,38 @@ class IdentificationController extends \RCFramework\BackController
         {
             $_SESSION['connexion_status'] = 'connected';
             $_SESSION['login'] = $utilisateurEntity->email();
+        }
+    }
+
+
+    public function executeSigninginform (HTTPRequest $request)
+    {
+
+    }
+
+
+    public function executeSigningin (HTTPRequest $request)
+    {
+        if (isset($_POST['prenom']) && isset($_POST['nom']) && isset($_POST['birthdate']) && isset($_POST['numero_rue']) && isset($_POST['nom_rue'])
+            && isset($_POST['code_postal']) && isset($_POST['ville']) && isset($_POST['pays']) && isset($_POST['email_adress']) && isset($_POST['password']))
+        {
+            $newUtilisateurEntity = new UtilisateurEntity();
+            $newUtilisateurEntity->setPrenom($_POST['prenom']);
+            $newUtilisateurEntity->setNom($_POST['nom']);
+            $newUtilisateurEntity->setBirthdate($_POST['birthdate']);
+            $newUtilisateurEntity->setNumero_rue($_POST['numero_rue']);
+            $newUtilisateurEntity->setNom_rue($_POST['nom_rue']);
+            $newUtilisateurEntity->setComplement_adresse($_POST['complement_adresse']);
+            $newUtilisateurEntity->setCode_postal($_POST['code_postal']);
+            $newUtilisateurEntity->setVille($_POST['ville']);
+            $newUtilisateurEntity->setPays($_POST['pays']);
+            $newUtilisateurEntity->setTelephone($_POST['telephone']);
+            $newUtilisateurEntity->setEmail($_POST['email_adress']);
+            $newUtilisateurEntity->setPassword(password_hash($_POST['password'], PASSWORD_BCRYPT));
+
+            $newUtilisateurManager = new UtilisateursManager();
+            $newUtilisateurManager->saveOneUtilisateur($newUtilisateurEntity);
+            
         }
     }
 }
