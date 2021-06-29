d798f85372ef4dfd5221e96d40b50db54a655108
Romain CHARLES <romain.charles@rocketmail.com>
2021-06-29 17:47:02 +0200

    Factorisation de l'identification visiteur et administrateur (en cours)

--- lib/RCFramework/Manager.php
+++ lib/RCFramework/Manager.php
@@ -33,4 +33,65 @@ class Manager
 
         return $this->managers[$module];
     }
+
+
+
+    public function compareIdentificationWithDb($login, $password, $requeteSQL, $correspondanceLoginDB, $userEntity)
+    {
+        if (!Utilitaires::emptyMinusZero($login) && !Utilitaires::emptyMinusZero($password))
+        {
+            $answerData = $this->db->prepare($requeteSQL);
+            $answerData->execute(array($correspondanceLoginDB => $login));
+            $dbUser = $answerData->fetch();
+
+            if ($dbUser === false)
+            {
+                return null;
+            }
+
+            $userFeatures = new $userEntity($dbUser);
+
+            if (password_verify($password, $userFeatures->password()))
+            {
+                return $userFeatures;
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+        return null;
+    }
+
+    /*
+    public function compareIdentificationWithDb($login, $password)
+    {
+        if (!Utilitaires::emptyMinusZero($login) && !Utilitaires::emptyMinusZero($password))
+        {
+            $answerUtilisateurData = $this->db->prepare('SELECT * FROM rc_photographe_utilisateurs WHERE utilisateurs_email=:utilisateurEmail');
+            $answerUtilisateurData->execute(array('utilisateurEmail' => $login));
+            $dbUtilisateur = $answerUtilisateurData->fetch();
+
+            if ($dbUtilisateur === false)
+            {
+                return null;
+            }
+
+            $utilisateurFeatures = new UtilisateurEntity($dbUtilisateur);
+
+            if (password_verify($password, $utilisateurFeatures->password()))
+            {
+                return $utilisateurFeatures;
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+        return null;
+    }
+    */
+
 }
