f025018678a325822f1f320014f761d51cfd0d68
Romain CHARLES <romain.charles@rocketmail.com>
2021-07-06 18:06:25 +0200

    Mise en place gestion numéros commandes et factures (en cours)

---
+++ lib/vendor/Entity/RangFactureCommandeEntity.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace Entity;
+
+
+use RCFramework\Entity;
+
+
+class RangFactureCommandeEntity extends Entity
+{
+    //    CLASS_PREFIX contient le préfixe de la table
+    protected const CLASS_PREFIX = 'rangs_commandes';
+    private string $numero_commande;
+    private string $numero_facture;
+
+    public function setNumeros_commandes($numero_commande)
+    {
+        $this->numero_commande = $numero_commande;
+    }
+
+    public function numero_commande():string
+    {
+        return $this->numero_commande;
+    }
+
+
+    public function setNumero_facture($numero_facture)
+    {
+        $this->numero_facture = $numero_facture;
+    }
+
+    public function numero_facture():string
+    {
+        return $this->numero_facture;
+    }
+
+}
---
+++ lib/vendor/Model/RangFactureCommandeManager.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Model;
+
+
+use Entity\PhotoEntity;
+use PDO;
+use RCFramework\Manager;
+
+class RangFactureCommandeManager extends Manager
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+
+}
