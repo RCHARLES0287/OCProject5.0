4cc6ed36bd5c60ffaac54edf851b078fcec1388f
Romain CHARLES <romain.charles@rocketmail.com>
2021-07-06 18:04:12 +0200

    Modification dans la gestion des messages d'erreurs

--- lib/RCFramework/Utilitaires.php
+++ lib/RCFramework/Utilitaires.php
@@ -6,23 +6,29 @@ namespace RCFramework;
 
 abstract class Utilitaires
 {
-    public static function emptyMinusZero ($value)
+    public static function emptyMinusZero($value)
     {
         if ($value == '')       //Contrôle les chaînes vides
         {
             return true;
         }
-        else if (is_string($value) && trim($value) === '')       //Contrôle les chaînes d'espaces blancs
-        {
-            return true;
-        }
-        else if ($value === null)
-        {
-            return true;
-        }
         else
         {
-            return false;
+            if (is_string($value) && trim($value) === '')       //Contrôle les chaînes d'espaces blancs
+            {
+                return true;
+            }
+            else
+            {
+                if ($value === null)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
         }
     }
 
@@ -33,25 +39,21 @@ abstract class Utilitaires
         echo '</pre>';
     }
 
-    public static function logException ($exception)
+    public static function logException($exception)
     {
-        $message = 'EXCEPTION ['.get_class($exception).'] : '.$exception->getMessage().' ('.$exception->getFile().':'.$exception->getLine().')';
+        $message = 'EXCEPTION [' . get_class($exception) . '] : ' . $exception->getMessage() . ' (' . $exception->getFile() . ':' . $exception->getLine() . ')';
         static::logMessage($message);
     }
 
-    public static function logMessage ($message)
+    public static function logMessage($message)
     {
-        file_put_contents(__DIR__.'/../../cache/erreur_'.date('Y-m-d').'.log', '['.date('H:i:s').'] '.$message.PHP_EOL, FILE_APPEND);
+        file_put_contents(__DIR__ . '/../../cache/erreur_' . date('Y-m-d') . '.log', '[' . date('H:i:s') . '] ' . $message . PHP_EOL, FILE_APPEND);
     }
 
-    public static function returnJsonAndExit ($value)
+    public static function returnJsonAndExit($value)
     {
         header('Content-Type: application/json; charset=utf-8');
-        if ($value["status"] !== 'Succès')
-        {
-            echo json_encode($value, JSON_THROW_ON_ERROR);
-            exit;
-        }
-
+        echo json_encode($value, JSON_THROW_ON_ERROR);
+        exit;
     }
 }
