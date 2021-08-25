348dd858be8c21a5f13cc1283ae02bb1a7fd5ece
Romain CHARLES <romain.charles@rocketmail.com>
2021-08-25 15:59:51 +0200

    Récupération d'une entité administrateur avec l'email

--- lib/vendor/Model/AdministrateurManager.php
+++ lib/vendor/Model/AdministrateurManager.php
@@ -14,7 +14,7 @@ class AdministrateurManager extends ManagerAuthentification
         return AdminEntity::class;
     }
 
-    public function getOneAdministrateur ($adminId)
+    public function getOneAdministrateurWithAdminId ($adminId)
     {
         $answerAdministrateurData = $this->db->prepare('SELECT * FROM rc_photographe_admins WHERE admins_id=:adminId');
         $answerAdministrateurData ->execute(array(
@@ -26,6 +26,25 @@ class AdministrateurManager extends ManagerAuthentification
         if ($dbAdministrateur === false)
         {
             throw new \Exception("Aucun administrateur ne porte l'Id " . $adminId);
+        }
+        else
+        {
+            return new AdminEntity($dbAdministrateur);
+        }
+    }
+
+    public function getOneAdministrateurWithEmail ($email)
+    {
+        $answerAdministrateurData = $this->db->prepare('SELECT * FROM rc_photographe_admins WHERE admins_login=:email');
+        $answerAdministrateurData ->execute(array(
+            'emailPassword' => $email
+        ));
+
+        $dbAdministrateur = $answerAdministrateurData->fetch();
+
+        if ($dbAdministrateur === false)
+        {
+            throw new \Exception("Aucun administrateur n'utilise l'email " . $email);
         }
         else
         {
