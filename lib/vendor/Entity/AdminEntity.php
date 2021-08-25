d5ef982f055627b37fa02f029fe11e1cd448d7e0
Romain CHARLES <romain.charles@rocketmail.com>
2021-08-25 15:58:27 +0200

    Ajout du password d'email pour l'administrateur

--- lib/vendor/Entity/AdminEntity.php
+++ lib/vendor/Entity/AdminEntity.php
@@ -5,10 +5,32 @@ namespace Entity;
 
 
 use RCFramework\EntityAuthentificationUser;
+use RCFramework\Utilitaires;
 
 class AdminEntity extends EntityAuthentificationUser
 {
     protected const CLASS_PREFIX = 'admins';
+
+    private ?string $email_password = null;
+
+    public function setEmail_password($emailPassword)
+    {
+        if (Utilitaires::emptyMinusZero($emailPassword))
+        {
+            throw new \Exception("Un mot de passe de messagerie doit être défini");
+        }
+        else
+        {
+            $this->email_password = $emailPassword;
+        }
+    }
+
+    public function email_password():string
+    {
+        return $this->email_password;
+    }
+
+
     /*private string $login;
     private string $password;*/
 
