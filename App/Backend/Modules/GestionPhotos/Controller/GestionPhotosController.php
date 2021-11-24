945a7a6848f3b9a537236443dab5d349ce3ae915
Romain CHARLES <romain.charles@rocketmail.com>
2021-11-24 18:04:50 +0100

    Mise en place gestion ordre des photos du carousel (à finir)

--- App/Backend/Modules/GestionPhotos/Controller/GestionPhotosController.php
+++ App/Backend/Modules/GestionPhotos/Controller/GestionPhotosController.php
@@ -2,7 +2,40 @@
 
 namespace App\Backend\Modules\GestionPhotos\Controller;
 
-class GestionPhotosController
-{
+use Model\GaleriesManager;
+use Model\PhotosManager;
+use RCFramework\HTTPRequest;
 
+class GestionPhotosController extends \RCFramework\BackController
+{
+    public function executeAddonephoto(HTTPRequest $request)
+    {
+        $newGaleriesManager = new GaleriesManager();
+        $allGaleries = $newGaleriesManager->getAllGaleries();
+
+        $this->page->addVar('allGaleries', $allGaleries);
+    }
+
+
+    public function executeAjoutphotoform(HTTPRequest $request)
+    {
+
+    }
+
+    public function executeOrdrecarousel(HTTPRequest $request)
+    {
+        var_dump('entrée dans controller de ordrecarousel');
+        exit;
+
+
+        $newPhotosManager = new PhotosManager();
+        $allPhotos = $newPhotosManager->getAllPhotos();
+
+        $newGaleriesManager = new GaleriesManager();
+        $allGaleries = $newGaleriesManager->getAllGaleries();
+
+        $this->page->addVar('allPhotos', $allPhotos);
+        $this->page->addVar('allGaleries', $allGaleries);
+
+    }
 }
