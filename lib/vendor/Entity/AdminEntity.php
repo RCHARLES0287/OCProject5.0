5d2346da9862c20d3b587d73cfed49767f2d1643
Romain CHARLES <romain.charles@rocketmail.com>
2022-01-04 20:03:33 +0100

    Suppression de la propriété email_password inutile

--- lib/vendor/Entity/AdminEntity.php
+++ lib/vendor/Entity/AdminEntity.php
@@ -11,24 +11,7 @@ class AdminEntity extends EntityAuthentificationUser
 {
     protected const CLASS_PREFIX = 'admins';
 
-    private ?string $email_password = null;
 
-    public function setEmail_password($emailPassword)
-    {
-        if (Utilitaires::emptyMinusZero($emailPassword))
-        {
-            throw new \Exception("Un mot de passe de messagerie doit être défini");
-        }
-        else
-        {
-            $this->email_password = $emailPassword;
-        }
-    }
-
-    public function email_password():string
-    {
-        return $this->email_password;
-    }
 
 
     /*private string $login;
